a46d2b96deda2e10163657fbb1c74e0d5246ca10
Janko13 <uki2003ns@gmail.com>
2022-03-22 07:40:20 +0100

    Kalendar

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.use(express.static('public'))
-
+app.use('/jquery', express.static(__dirname+"/node_modules/jquery/dist/"));
 app.get('/', (req, res) => {
   res.sendFile(__dirname+'/public/home.html');
 });
